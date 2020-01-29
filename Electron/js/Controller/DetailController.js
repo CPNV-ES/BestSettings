bd9538c5393966a3fbeeb507b13f592f43d7a944
VIQUERAT Killian <Killian.VIQUERAT@cpnv.ch>
2020-01-29 12:20:31 +0100

    add js for showing graphical settings

--- Electron/js/Controller/DetailController.js
+++ Electron/js/Controller/DetailController.js
@@ -1,9 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
-const ipc = require('electron').ipcRenderer; 
+const ipc = require('electron').ipcRenderer;
 let serveur = ipc.sendSync('serveur');
 const id = urlParams.get('id');
 
-Element.prototype.addElement = function(type, classlist){
+Element.prototype.addElement = function (type, classlist) {
     var elem = document.createElement(type);
     elem.classList = classlist;
     this.appendChild(elem);
@@ -11,42 +11,85 @@ Element.prototype.addElement = function(type, classlist){
 }
 
 $.ajax({
-    url: serveur+'/game/'+id,
+    url: serveur + '/game/' + id,
     data: '',
-    error: function() {
-        document.getElementById("error").innerText="An error has occurred";
+    error: function () {
+        document.getElementById("error").innerText = "An error has occurred";
     },
     dataType: 'json',
-    success: function(data) {
+    success: function (data) {
         AddInformationGame(data);
+        AddTableGraphic(data);
+        AddTableSettings(data);
     },
     type: 'GET'
 });
 
-function AddInformationGame(data){
-    var divContainLogo = gameInfo.addElement('div','col-4');
-    var divLogo = divContainLogo.addElement('div','gameLogo');
-    var url = serveur+data.logo;
-    divLogo.style.backgroundImage ="url("+url+")";
-    var divGameInfo = gameInfo.addElement('div','col-6 mt-2 float-right');
-    data.gameCategories.forEach(element => {
-        var divCategory = divGameInfo.addElement('div','col-6');
+function AddInformationGame(data) {
+    var divContainLogo = gameInfo.addElement('div', 'col-4');
+    var divLogo = divContainLogo.addElement('div', 'gameLogo');
+    var url = serveur + data.logo;
+    divLogo.style.backgroundImage = "url(" + url + ")";
+    var divGameInfo = gameInfo.addElement('div', 'col-6 mt-2 float-right');
+    data.gamesCategories.forEach(element => {
+        var divCategory = divGameInfo.addElement('div', 'col-6');
         divCategory.innerText = element.name;
-        var img = divCategory.addElement('img','categoryLogo pl-2');
-        var url = serveur+element.logo;
-        img.src= url;
+        var img = divCategory.addElement('img', 'categoryLogo pl-2');
+        var url = serveur + element.logo;
+        img.src = url;
         img.width = "24";
         img.height = "24";
     });
     data.platforms.forEach(element => {
-        var divPlatform = divGameInfo.addElement('div','col-6');
+        var divPlatform = divGameInfo.addElement('div', 'col-6');
         divPlatform.innerText = element.name;
-        var img = divPlatform.addElement('img','platform pl-2');
-        var url = serveur+element.logo;
-        img.src= url;
+        var img = divPlatform.addElement('img', 'platform pl-2');
+        var url = serveur + element.logo;
+        img.src = url;
         img.width = "24";
         img.height = "24";
     });
     gameInfo.addElement('hr');
     document.getElementById("gameInfo");
+}
+
+function AddTableGraphic(data) {
+    settings = data.gameConfigurations[0].graphicsConfigs.settings[0];
+    row = createRow(settings);
+    Object.keys(row).forEach(key => {
+        data = row[key];
+        var tr = graphicsettings.addElement('tr');
+        var td = tr.addElement('td');
+        td.innerText = key;
+        Object.keys(data).forEach(key => {
+
+            var td = tr.addElement('td');
+            td.innerText = data[key];
+        });
+    });
+
+    /*<tr>
+        <td>Resolution</td>
+        <td>1920x1080</td>
+        <td>1920x1080</td>
+    </tr>*/
+}
+
+function AddTableSettings(data) {
+
+}
+
+function createRow(settings) {
+    var parameter = {};
+    var keys = Object.keys(settings);
+    Object.keys(settings).forEach(key => {
+        params = settings[key][0];
+        Object.keys(params).forEach(key => {
+            if (parameter[key] == null) {
+                parameter[key] = [];
+            }
+            parameter[key].push(params[key]);
+        });
+    });
+    return parameter;
 }
--- Electron/js/Controller/GameController.js
+++ Electron/js/Controller/GameController.js
@@ -44,7 +44,6 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         data.forEach(element => {
-            console.log(element);
             var a = menuContent.addElement('a', '');
             a.href= "";
             a.innerText= element.name;
